02a2006237509cbaa157392ee1775f6f0c4acd08
Suvrodev Howlader <suvrodev.cse@yahoo.com>
2023-05-24 15:04:08 +0600

    Added Helmet

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -4,10 +4,15 @@ import Category from '../Categories/Category';
 import PopularMenu from '../PopularMenu/PopularMenu';
 import Featured from '../Featured/Featured';
 import Testimonials from '../Testimonial/Testimonials';
+import { Helmet } from 'react-helmet-async';
 
 const Home = () => {
     return (
         <div>
+             <Helmet>
+                <title>Home | Bistro Boss</title>
+            </Helmet>
+
             This is Home
             <Banner></Banner>
             <Category></Category>
--- src/main.jsx
+++ src/main.jsx
@@ -8,12 +8,16 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import router from './Routes/Routes.jsx';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
    <div className='max-w-7xl mx-auto'>
-      <React.StrictMode>
-      <RouterProvider router={router} />
-     </React.StrictMode>
+      <HelmetProvider>
+          <React.StrictMode>
+             <RouterProvider router={router} />
+          </React.StrictMode>
+      </HelmetProvider>
    </div>
 )
